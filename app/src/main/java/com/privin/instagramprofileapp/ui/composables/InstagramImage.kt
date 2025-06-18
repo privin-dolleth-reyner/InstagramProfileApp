cb7289980250488e30d0247722b35d8a2724cfb8
Privin Dolleth Reyner P <privin@privin.dev>
2025-06-18 11:15:46 +0530

    Story highlights done

--- app/src/main/java/com/privin/instagramprofileapp/ui/composables/InstagramImage.kt
+++ app/src/main/java/com/privin/instagramprofileapp/ui/composables/InstagramImage.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,19 +19,25 @@ import com.privin.instagramprofileapp.R
 
 @Composable
 fun InstagramImage(image: Painter, isHighlights: Boolean, modifier: Modifier = Modifier){
-    val conditionalModifier = modifier
-        .aspectRatio(1f, matchHeightConstraintsFirst = true)
-    
-    if(isHighlights){
-        conditionalModifier.border(width = 1.dp, color = Color.LightGray, shape = CircleShape)
-            .padding(2.dp)
-    }
-
     Image(
         painter = image,
         contentDescription = null,
-        modifier = conditionalModifier
-            .clip(CircleShape)
+        contentScale = ContentScale.Crop,
+        modifier = if(isHighlights) {
+            modifier
+                .aspectRatio(1f, matchHeightConstraintsFirst = true)
+                .border(
+                    width = 1.dp,
+                    color = Color.LightGray,
+                    shape = CircleShape
+                )
+                .padding(4.dp)
+                .clip(CircleShape)
+        }else{
+            modifier
+                .aspectRatio(1f, matchHeightConstraintsFirst = true)
+                .clip(CircleShape)
+        }
     )
 }
 
---
+++ app/src/main/java/com/privin/instagramprofileapp/ui/composables/StoryHighlights.kt
@@ -0,0 +1,74 @@
+package com.privin.instagramprofileapp.ui.composables
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.privin.instagramprofileapp.R
+
+data class Story(@DrawableRes val image: Int, val title: String)
+
+@Composable
+fun StoryHighlights(
+    stories: List<Story>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(modifier = modifier) {
+        items(stories) {
+            Column {
+                InstagramImage(
+                    image = painterResource(it.image),
+                    isHighlights = true,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(4.dp)
+                )
+
+                Text(
+                    text = it.title,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun StoryHighlightsPreview() {
+    StoryHighlights(
+        stories = listOf(
+            Story(R.mipmap.story_1,"Portugal"),
+            Story(R.mipmap.story_2,"FIFA"),
+            Story(R.mipmap.story_3,"League"),
+            Story(R.mipmap.story_4,"World cup"),
+            Story(R.mipmap.story_5,"FIFA"),
+            Story(R.mipmap.story_6,"League"),
+            Story(R.mipmap.story_7,"World cup"),
+            Story(R.mipmap.story_8,"FIFA"),
+            Story(R.mipmap.story_9,"Portugal"),
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.White)
+            .padding(8.dp)
+    )
+}
