dc7901af36132c0a18f805e69d1ae1e2f429906c
Privin Dolleth Reyner P <privin@privin.dev>
2025-06-18 10:26:41 +0530

    Follow and message button done

--- app/src/main/java/com/privin/instagramprofileapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/privin/instagramprofileapp/ui/screens/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.privin.instagramprofileapp.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +11,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.privin.instagramprofileapp.R
+import com.privin.instagramprofileapp.ui.composables.ProfileActions
 import com.privin.instagramprofileapp.ui.composables.ProfileHeader
 import com.privin.instagramprofileapp.ui.composables.ProfileTopBar
 
 @Composable
 fun ProfileScreen(userName: String, name: String, bio: String, modifier: Modifier = Modifier){
-    Column(modifier = modifier.fillMaxSize().padding(horizontal = 16.dp)) {
+    Column(modifier = modifier.padding(horizontal = 16.dp)) {
         ProfileTopBar(
             title = userName,
             isVerified = true,
@@ -51,6 +54,10 @@ fun ProfileScreen(userName: String, name: String, bio: String, modifier: Modifie
             textAlign = TextAlign.Start,
             modifier = Modifier
         )
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileActions(
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
@@ -60,7 +67,10 @@ fun ProfileScreenPreview(){
     ProfileScreen(
         userName = "cristiano",
         name = "Cristiano Ronaldo",
-        bio = "SIUUUbscribe to my Youtube Channel!"
+        bio = "SIUUUbscribe to my Youtube Channel!",
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
     )
 }
 
